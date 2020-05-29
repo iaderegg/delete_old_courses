ddd822256eb2a916b82fc37d881414773a396ecc
Iader E. Garcia G <iadergg@gmail.com>
2020-05-29 16:33:13 -0500

    + Se crea libreria para la gestión de notificaciones por correo electrónico + Se envia un correo electrónico cada vez que se ejecute el script, notificando cuandos cursos quedan pendientes por eliminar. + Se configura validación para la finalización del script a las 01:00 horas y a las 07:00 horas.

---
+++ lib/lib_mail.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Lib for email messages.
+ * @author Iader E. Garcia Gomez <iader.garcia@correounivalle.edu.co>
+ * Version: 0.1
+ **/
+
+function delete_old_courses_send_email( $usernameTo, $usernameFrom, $coursesToDelete) {
+
+    $fromUser = core_user::get_user_by_username(
+                                        $usernameFrom,
+                                        'id, 
+                                        firstname, 
+                                        lastname, 
+                                        username, 
+                                        email, 
+                                        maildisplay, 
+                                        mailformat,
+                                        firstnamephonetic,
+                                        lastnamephonetic,
+                                        middlename,
+                                        alternatename'
+                                    );
+
+    $toUser = core_user::get_user_by_username(
+                                        $usernameTo,
+                                        'id, 
+                                        firstname, 
+                                        lastname, 
+                                        username, 
+                                        email, 
+                                        maildisplay, 
+                                        mailformat,
+                                        firstnamephonetic,
+                                        lastnamephonetic,
+                                        middlename,
+                                        alternatename'
+                                    );
+
+    
+    $subject = "Notificación sobre cursos pendientes por borrar en el Campus Virtual";
+
+    $textToSendHtml = "El módulo de eliminación de cursos ha detectado que el día de hoy quedan cursos pendientes por borrar.<br><br>";
+    $textToSendHtml .= "Cantidad de cursos pendientes: " . $coursesToDelete . "<br><br>";
+    $textToSendHtml .= "Este mensaje ha sido generado automáticamente, por favor no responda a este mensaje.";
+
+    $textToSend = html_to_text($textToSendHtml);
+
+    echo $textToSend;
+
+    $completeFilePath = "/home/admincampus/";
+
+    if (intval(date('H')) >= 1 && intval(date('H')) < 4) {
+        $nameFile = 'log_delete_courses_00';
+    } elseif (intval(date('H')) >= 7) {
+        $nameFile = 'log_delete_courses_04';
+    }
+
+    $resultSendMessage = email_to_user($toUser, $fromUser, $subject, $textToSend, $textToSendHtml, $completeFilePath, $nameFile, true);
+}
